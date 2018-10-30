83d6ee99503393404fc19126af5ce460a71f11b8
Sam Bennrubi <sbennrubi@gmail.com>
2018-10-30 23:54:38 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,4 +2,8 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator
+def batch_badge_creator(array)
+  array.each do |name|
+    puts "Hello, my name is #{name}."
+  end
+end
