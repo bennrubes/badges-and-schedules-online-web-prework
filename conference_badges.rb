14633e2e2f463848360e4db770140ddc8812c23e
Sam Bennrubi <sbennrubi@gmail.com>
2018-10-31 00:37:33 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,6 +19,6 @@ def assign_rooms(array)
 end
 
 def printer(array)
-  puts batch_badge_creator(array).chomp
-  puts assign_rooms(array).chomp
+  puts batch_badge_creator(array)
+  puts assign_rooms(array)
 end
