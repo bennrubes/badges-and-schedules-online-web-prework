979fb142a2037ada1141d514ce3b2231f0d48be3
Sam Bennrubi <sbennrubi@gmail.com>
2018-10-31 00:20:42 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 def assign_rooms(array)
   list_assignment = []
   array.each do |name|
-    list_assignment.push("Hello #{name}! You'll be assigned to room [num]")
+    list_assignment.push("Hello #{name}! You'll be assigned to room #{name.index]")
   end
   list_assignment
 end
